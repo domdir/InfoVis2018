fe1745412485ef5203c06a2c88b373cbcc97ca54
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2018-05-20 17:12:31 +0200

    Test d3.js

--- src/js/visualisation.js
+++ src/js/visualisation.js
@@ -1,4 +1,27 @@
 // load data
-d3.json("./assets/dump.json").then(function(data) {
-  console.log(data);
+d3.json("./assets/dump-small.json").then(function(data) {
+    console.log(data);
+
+    packages = data['packages']; // extract packages from data
+
+    // create traffic graph
+    let svgContainer = d3
+        .select("#traffic_overview")
+        .append("svg")
+        .attr("width", 800)
+        .attr("height", 200)
+        .style("border", "1px solid black");
+
+    // simply print some traffic informations to test d3js
+    d3.select("#traffic_overview")
+        .selectAll("p")
+        .data(packages)
+        .enter()
+        .append("p")
+        .text(function (d, i) {
+            console.log(d);
+            console.log(i);
+            return "pgk: " + d['src_ip'] + " -> " + d['dst_ip'];
+        });
+
 });
