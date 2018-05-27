47b339fd63ce5fdba83d4be422a4107cd1cae4df
Dominik Dirmeier <dominik.dirmeier@outlook.at>
2018-05-27 22:31:21 +0200

    updated code structure

--- src/js/visualisation.js
+++ src/js/visualisation.js
@@ -1,142 +1,167 @@
+/// ###########################################
+/// ############ GLOBALS ######################
+/// ###########################################
+
+const dimensions = {
+    trafficOverview: {
+        margin: {
+            top: 20,
+            right: 20,
+            bottom: 30,
+            left: 50
+        },
+        width: null,
+        height: null
+    }
+};
+
+const elements = {
+    trafficOverview: {
+
+    }
+};
+
+const model = {
+    full: null,
+    trafficOverview: {
+        cells: null
+    }
+};
+
+/// ###########################################
+/// ############ INITIALIZERS #################
+/// ###########################################
+
 // load data
 d3.json("./assets/dump-small.json").then(function (data) {
     console.log(data);
+    model.full = data;
 
-    packages = data['packages']; // extract packages from data
+    updateDimensions();
 
-    //let groupedPackages = packages.reduce((acc, val) => {
-    //    acc[val.src_ip] = acc[val.src_ip] || [];
-    //    acc[val.src_ip].push(val);
-    //    return acc;
-    //}, {});
+    initTrafficOverview();
+});
 
-    //let summedGroupedPackages = {};
-    //for (let i in groupedPackages) {
-    //    summedGroupedPackages[i] = groupedPackages[i].reduce((acc, val) => {
-    //        let timestamp = Math.round(val.timestamp * 10);
-    //        acc[timestamp] = acc[timestamp] || { packages: [], pkg_size: 0, timestamp: timestamp / 10 };
-    //        acc[timestamp].packages.push(val);
-    //        acc[timestamp].pkg_size += val.pkg_size;
-    //        return acc;
-    //    });
-    //}
-
-    //console.log(summedGroupedPackages);
-
-    //let sumPkgSize = [];
-
-    //let summedGroupedPackages = {};
-    //for (let i in groupedPackages) {
-    //    summedGroupedPackages[i] = {};
-    //    groupedPackages[i].forEach((val) => {
-    //        let timestamp = Math.round(val.timestamp * 10);
-    //        summedGroupedPackages[i][timestamp] = summedGroupedPackages[i][timestamp] || { packages: [], pkg_size: 0, timestamp: timestamp / 10 };
-    //        summedGroupedPackages[i][timestamp].packages.push(val);
-    //        summedGroupedPackages[i][timestamp].pkg_size += val.pkg_size;
-    //        sumPkgSize[timestamp] = sumPkgSize[timestamp] + val.pkg_size || val.pkg_size;
-    //    });
-    //}
-    //console.log(summedGroupedPackages);
-
-    let ips = {};
-
-    let summedData = {};
-    for (let i in packages) {
-        let timestamp = Math.round(packages[i].timestamp * 10);
-        summedData[timestamp] = summedData[timestamp] || { timestamp: timestamp / 10, total_pkg_size: 0 };
-        summedData[timestamp][packages[i].src_ip] = summedData[timestamp][packages[i].src_ip] + packages[i].pkg_size || packages[i].pkg_size;
-        summedData[timestamp].total_pkg_size += packages[i].pkg_size;
-        ips[packages[i].src_ip] = 1;
-    }
-
-    summedData = Object.values(summedData); //required for the stack iterator
-    console.log(summedData);
-
-    let keys = Object.keys(ips);
-
-
-    let margin = { top: 5, right: 5, bottom: 5, left: 5 },
-        width = 800 - margin.left - margin.right,
-        height = 200 - margin.top - margin.bottom;
-
+function initTrafficOverview() {
     // create traffic graph
-    let svgContainer = d3
+    const eles = elements.trafficOverview;
+
+    eles.svg = d3
         .select("#traffic_overview")
         .append("svg")
-        .attr("width", 800)
-        .attr("height", 200)
+        .attr('width', dimensions.trafficOverview.width + dimensions.trafficOverview.margin.left + dimensions.trafficOverview.margin.right)
+        .attr('height', dimensions.trafficOverview.height + dimensions.trafficOverview.margin.top + dimensions.trafficOverview.margin.bottom)
         .style("border", "1px solid black");
 
-    let g = svgContainer.append('g')
-        .attr('transform', `translate(${margin.left},${margin.top})`);
+    eles.g = eles.svg.append("g")
+        .attr("transform", `translate(${dimensions.trafficOverview.margin.left},${dimensions.trafficOverview.margin.top})`);
 
-    let xscale = d3.scaleTime().range([0, width]);
-    let yscale = d3.scaleLinear().range([0, height]);
+    eles.x = d3.scaleTime()
+        .rangeRound([0, dimensions.trafficOverview.width]);
+    eles.y = d3.scaleLinear()
+        .rangeRound([dimensions.trafficOverview.height, 0]);
 
-    let xaxis = d3.axisTop().scale(xscale);
-    let g_xaxis = g.append('g').attr('class', 'x axis');
-    let yaxis = d3.axisLeft().scale(yscale);
-    let g_yaxis = g.append('g').attr('class', 'y axis');
+    eles.xaxis = d3.axisBottom().scale(eles.x);
+    eles.g_xaxis = eles.g.append("g")
+        .attr("class", "x axis")
+        .attr("transform", `translate(0, ${dimensions.trafficOverview.height})`);
+    eles.yaxis = d3.axisLeft().scale(eles.y);
+    eles.g_yaxis = eles.g.append('g').attr('class', 'y axis');
 
-    let colors = d3.scaleOrdinal(d3.schemeCategory10);
+    updateTrafficOverview();
+}
 
-    xscale.domain([d3.min(summedData, (d) => d.timestamp), d3.max(summedData, (d) => d.timestamp)]);
-    yscale.domain([0, d3.max(summedData, (d) => d.total_pkg_size)]);
-    colors.domain();
+/// ###########################################
+/// ############ UPDATE FUNCTIONS #############
+/// ###########################################
 
-    g_xaxis.call(xaxis);
-    g_yaxis.call(yaxis);
+function updateDimensions() {
+    dimensions.trafficOverview.width = 960 - dimensions.trafficOverview.margin.left - dimensions.trafficOverview.margin.right;
+    dimensions.trafficOverview.height = 500 - dimensions.trafficOverview.margin.top - dimensions.trafficOverview.margin.bottom;
+}
 
-    let stack = d3.stack();
+function updateTrafficOverview() {
+    let packets = model.full.packages;
 
-    let area = d3.area()
-        .x((d, i) => xscale(d.data.timestamp))
-        .y0((d) => yscale(d[0]))
-        .y1((d) => yscale(d[1]));
+    let interval_min = Math.min.apply(Math, packets.map(x => x.timestamp));
+    let interval_max = Math.max.apply(Math, packets.map(x => x.timestamp));
+    let duration = interval_max - interval_min;
 
-    stack.keys(keys)
+    console.log("interval: " + interval_min + " - " + interval_max + " [dur=" + duration + "]");
 
-    let layer = g.selectAll('.layer')
-        .data(stack(summedData));
+    // aggregate troughput
+    let cell_count = Math.ceil(duration);
 
-    let layer_enter = layer.enter()
-        .append('g').attr('class', 'layer');
+    if (Math.ceil(interval_min) == Math.ceil(interval_max)) {
+        cell_count = 1;
+    }
 
-    layer_enter.append('path')
-        .attr('class', 'area')
-        .style('fill', (d) => colors(d.key))
-        .attr('d', area);
+    console.log("cell count: " + cell_count);
 
-    layer_enter
-        .append("text")
-        .attr("x", width - 6)
-        .attr("y", function (d) { return yscale((d[d.length - 1][0] + d[d.length - 1][1]) / 2); })
-        .attr("dy", ".35em")
-        .style("font", "10px sans-serif")
-        .style("text-anchor", "end")
-        .text(function (d) { return d.key; });
+    let cells = new Array(cell_count);
+    for (let i = 0; i < cells.length; i++) {
+        let begin = Math.floor(interval_min);
+        cells[i] = {
+            begin: begin + i,
+            end: begin + i + 1,
+            center: begin + i + 0.5,
+            date: new Date((begin + i + 0.5) * 1000),
+            value: 0
+        };
+    }
 
-    g.append("g")
-        .attr("class", "axis axis--x")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(xscale));
+    let last = 0;
+    for (let i = 0; i < cell_count; i++) {
+        cells[i].value = 0;
+        for (let j = last; j < packets.length; j++) {
+            let packet = packets[j];
+            if (packet.timestamp >= cells[i].end) {
+                break;
+            } else {
+                last = j;
+                cells[i].value += packet.pkg_size;
+            }
+        }
+    }
 
-    g.append("g")
-        .attr("class", "axis axis--y")
-        .call(d3.axisLeft(yscale).ticks(1, "%"));
-    
-    // simply print some traffic informations to test d3js
-    //d3.select("#traffic_overview")
-    //    .selectAll("p")
-    //    .data(packages)
-    //    .enter()
-    //    .append("p")
-    //    .text(function (d, i) {
-    //        console.log(d);
-    //        console.log(i);
-    //        return "pgk: " + d['src_ip'] + " -> " + d['dst_ip'];
-    //    });
+    cells[0].begin = interval_min;
+    cells[cell_count - 1].end = interval_max;
 
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].value /= (cells[i].end - cells[i].begin); // normalize to bytes/sec
+    }
 
+    console.log(cells);
+    model.trafficOverview.cells = cells;
 
-});
+    renderTrafficOverview();
+}
+
+/// ###########################################
+/// ############ RENDER FUNCTIONS #############
+/// ###########################################
+
+function renderTrafficOverview () {
+    const eles = elements.trafficOverview;
+
+    // draw graph
+    eles.x.domain(d3.extent(model.trafficOverview.cells, d => d.date));
+    eles.y.domain([0, d3.max(model.trafficOverview.cells, d => d.value)]);
+
+    eles.g_xaxis.call(eles.xaxis);
+    eles.g_yaxis.call(eles.yaxis);
+
+    const line = d3.line()
+        .x(d => eles.x(d.date))
+        .y(d => eles.y(d.value));
+
+    // TODO: enter/merge/exit?
+    eles.g.append("path")
+        .datum(model.trafficOverview.cells)
+        .attr("fill", "none")
+        .attr("stroke", "black")
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+        .attr("stroke-width", 1.5)
+        .attr("d", line);
+}
